a858909b2638884e692d52b544da3b1b37e029e1
Shariar Islam <shariarn85@gmail.com>
2024-05-16 22:55:44 +0600

    Install Prettier,Ts-Node-Dev,Fix Formatting Issues

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 // const port = process.env.PORT;
 
-// parser
+//             parser
+
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   const a = 10;
+
   res.send(a);
 });
 
