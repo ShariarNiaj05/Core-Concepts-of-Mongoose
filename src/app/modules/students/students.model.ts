08f5096293fba485b67109b4a0a7fc3cd596283f
Shariar Islam <shariarn85@gmail.com>
2024-05-17 00:47:43 +0600

    Refactor Schema

--- src/app/modules/students/students.model.ts
+++ src/app/modules/students/students.model.ts
@@ -1,13 +1,30 @@
-import { Schema, model, connect } from 'mongoose';
-import { IStudent } from './students.interface';
+import { Schema, model } from 'mongoose';
+import { Guardian, IStudent, LocalGuardian, Name } from './students.interface';
+
+const userNameSchema = new Schema<Name>({
+  firstName: { type: String, required: true },
+  middleName: { type: String, required: false },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+});
 
 const studentSchema = new Schema<IStudent>({
   id: { type: String },
-  name: {
-    firstName: { type: String, required: true },
-    middleName: { type: String, required: false },
-    lastName: { type: String, required: true },
-  },
+  name: userNameSchema,
   gender: ['male', 'female'],
   dateOfBirth: { type: String, required: true },
   email: { type: String, required: true },
@@ -16,20 +33,12 @@ const studentSchema = new Schema<IStudent>({
   bloodGroup: ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'],
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: {
-    fatherName: { type: String, required: true },
-    fatherOccupation: { type: String, required: true },
-    fatherContactNo: { type: String, required: true },
-    motherName: { type: String, required: true },
-    motherOccupation: { type: String, required: true },
-    motherContactNo: { type: String, required: true },
-  },
-  localGuardian: {
-    name: { type: String, required: true },
-    occupation: { type: String, required: true },
-    contactNo: { type: String, required: true },
-    address: { type: String, required: true },
-  },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
   profileImage: { type: String, required: false },
   isActive: ['active', 'blocked'],
 });
+
+const Student = model<IStudent>('Student', studentSchema);
+
+export default Student;
